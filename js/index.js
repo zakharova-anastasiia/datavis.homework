203a80bdd7b36d6373de1f3698a9ea8547c560c4
zakharova-anastasiia <nastyazakharova.nz@gmail.com>
2021-06-05 12:26:02 +0300

    minor changes

--- js/index.js
+++ js/index.js
@@ -86,51 +86,50 @@ loadData().then(data => {
       regions = d3.set(data.map(d=>d.region)).values();
       let colorScale = d3.scaleOrdinal().domain(regions).range(['#DD4949', '#39CDA1', '#FD710C', '#A14BE5']);
 
-      var dict_r = [];
+      let dict_r = [];
       regions.forEach(function(r){
-        values = data.filter(function(d){return d.region == r;});
+        values = data.filter(d => d.region == r);
         dict_r.push({ 'region' : r, 'values' : d3.mean(values, d => d[param][year])});
       });
 
       let y = d3.scaleLinear().domain([d3.min(dict_r, d => d.values),
         d3.max(dict_r, d => +d.values) + 0.5* d3.min(dict_r, d => d.values)
       ]).range([height-margin, margin]);
-
       let x = d3.scaleBand().domain(regions).range([margin*2, barWidth-margin]).padding(0.1);
       xBarAxis.call(d3.axisBottom().scale(x));
       yBarAxis.call(d3.axisLeft().scale(y));
 
       barChart.selectAll('rect').remove();
 
-      var bars = barChart.append('g').selectAll("rect")
+      barChart.append('g').selectAll('rect')
       .data(dict_r)
-      .enter().append("rect")
-      .attr("region", d => d.region)
-      .style("fill", d => colorScale(d.region))
-      .attr("x", d => x(d.region))
-      .attr("width", '80')
-      .attr("y", d => y(d.values) - margin)
-      .attr("height", d => height - y(d.values))
-      .on("click", function(d) {
+      .enter().append('rect')
+      .attr('region', d => d.region)
+      .style('fill', d => colorScale(d.region))
+      .attr('x', d => x(d.region))
+      .attr('width', '90')
+      .attr('y', d => y(d.values) - margin)
+      .attr('height', d => height - y(d.values))
+      .on('click', function(d) {
         barChart.selectAll('rect')
                 .transition()
                 .attr('fill-opacity', 0.5);
         d3.select(this)
           .transition()
           .attr('fill-opacity', 1);
-        let col = d3.select(this).attr("region");
+        let col = d3.select(this).attr('region');
         scatterPlot.selectAll('circle')
-                   .filter(function(d) { return d.region != col; })
+                   .filter(d => d.region != col)
                    .style('fill-opacity', 0)
                    .style('opacity', 0);
         scatterPlot.selectAll('circle')
-                   .filter(function(d) { return d.region == col; })
+                   .filter(d => d.region == col)
                    .style('fill-opacity', 0.7)
                    .style('opacity', 1);
 
             })
-      .on("mouseout", function(d) {
-        barChart.selectAll("rect").transition()
+      .on('mouseout', function(d) {
+        barChart.selectAll('rect').transition()
           .attr('fill-opacity', 1);
         scatterPlot.selectAll('circle')
                    .transition()
@@ -164,7 +163,7 @@ loadData().then(data => {
 
       scatterPlot.selectAll('circle').remove();
 
-      var circles = scatterPlot.append('g').selectAll('circle')
+      scatterPlot.append('g').selectAll('circle')
       .data(data).enter()
       .append('circle')
       .attr('region', d => d.region)
@@ -173,19 +172,19 @@ loadData().then(data => {
       .attr('fill',  d => colorScale(d.region))
       .attr('cx', d => x(d[xParam][year]))
       .attr('cy', d => y(d[yParam][year]))
-      .attr("stroke-width", 0.4)
+      .attr('stroke-width', 0.4)
       .on('click', function(d) {
             d3.selectAll('circle')
                    .style('fill-opacity', 0.7)
                    .style('opacity', 1)
-                   .style("stroke-width", 0.4);
+                   .style('stroke-width', 0.4);
             d3.select(this).moveToFront();
             d3.select(this).transition()
             .style('fill-opacity', 1)
             .style('opacity', 1)
-            .style("stroke", "black")
-            .style("stroke-width", 1);
-            selected = d3.select(this).attr("country");
+            .style('stroke', 'black')
+            .style('stroke-width', 1);
+            selected = d3.select(this).attr('country');
             updateLineChart();
             countryName.html(selected);
 
@@ -195,7 +194,7 @@ loadData().then(data => {
                      .transition()
                      .style('fill-opacity', 0.7)
                      .style('opacity', 1)
-                     .style("stroke-width", 0.4);
+                     .style('stroke-width', 0.4);
               });
 
 
@@ -210,10 +209,10 @@ loadData().then(data => {
 
       if (selected != ''){
 
-        var country = data.find(d => (d.country == selected));
-        var years = Object.keys(country[lineParam]).slice(0, 221)
+        let country = data.find(d => (d.country == selected));
+        let years = Object.keys(country[lineParam]).slice(0, 221)
 
-        var dict_c = [];
+        let dict_c = [];
         years.forEach(function(d){
             dict_c.push({'year' : d3.timeParse("%Y")(d), 'value' : Number(country[lineParam][d])});
             return;
@@ -221,25 +220,24 @@ loadData().then(data => {
 
         let extent_x = d3.extent(dict_c, d => +d.year);
         let extent_y = d3.extent(dict_c, d => +d.value);
-
         let x = d3.scaleTime().domain(extent_x).range([margin*2, width-margin]);
         let y = d3.scaleLinear().domain(extent_y).range([height-margin, margin]);
         xLineAxis.call(d3.axisBottom().tickFormat(d3.timeFormat("%Y")).scale(x));
         yLineAxis.call(d3.axisLeft().scale(y));
 
 
-        lineChart.select(".line_data").remove();
+        lineChart.select('.line_data').remove();
 
-        lineChart.append("g")
-        .append("path")
+        lineChart.append('g')
+        .append('path')
         .data([dict_c])
-        .attr("class", "line_data")
-        .attr("stroke", 'rgb(36, 138, 181)')
-        .attr("d", d3.line()
+        .attr('class', 'line_data')
+        .attr('stroke', 'rgb(36, 138, 181)')
+        .attr('d', d3.line()
             .x(d => x(d.year))
             .y(d => y(d.value)))
-        .attr("stroke-width", 2)
-        .style("fill", "none");
+        .attr('stroke-width', 2)
+        .style('fill', 'none');
 
       };
         return;
